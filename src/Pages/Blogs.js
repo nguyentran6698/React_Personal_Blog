6e25b1fe885d135440f0b9002804e8ddac3cf45c
nguyen tran <nguyen.tran6698@gmail.com>
2022-05-01 22:45:20 -0500

    Adding Pagination

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -6,8 +6,7 @@ import BasicPost from "../components/PostTemplate/BasicPost";
 import { devices } from "../styled-components/size";
 import Header from "../components/PostHeaderSlide/BlogPostHeader";
 import { useSearchParams, Link } from "react-router-dom";
-import useFetch from "../customHook/useFetch";
-import { textAlign } from "@mui/system";
+import ReactPaginate from "react-paginate";
 // Testing Purspose
 const mapValueJsonServer = {
   categories: "_like",
@@ -16,10 +15,30 @@ const mapValueJsonServer = {
   title: "_like",
 };
 const Blogs = () => {
+  window.scrollTo(0, 0);
   const { loading, blogs, setQuery } = useGlobalContext();
   const [currentParams, setCurrentParams] = useState({});
   const [searchParams] = useSearchParams();
-
+  // PagePaginate
+  const itemsPerPage = 10;
+  const [currentItems, setCurrentItems] = useState(blogs);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  useEffect(() => {
+    if (blogs.length !== 0) {
+      const endOffset = itemOffset + itemsPerPage;
+      setCurrentItems(blogs.slice(itemOffset, endOffset));
+      setPageCount(Math.ceil(blogs.length / itemsPerPage));
+    }
+  }, [itemOffset, itemsPerPage, blogs]);
+  const handlePageClick = (e) => {
+    const newOffSet = (e.selected * itemsPerPage) % blogs.length;
+    console.log(
+      `User requested page number ${e.selected}, which is offset ${newOffSet}`
+    );
+    setItemOffset(newOffSet);
+  };
+  // HandleFetching
   useEffect(() => {
     if (currentParams.categories !== "all") {
       let queryParse = Object.keys(currentParams).map((key) => {
@@ -38,6 +57,7 @@ const Blogs = () => {
       ...Object.fromEntries([...searchParams]),
     });
   }, [searchParams]);
+
   if (loading) {
     return <Loading />;
   }
@@ -69,10 +89,31 @@ const Blogs = () => {
           </div>
         )}
         <div className="blog-post-section">
-          {blogs.map((blog, id) => {
+          {currentItems.map((blog, id) => {
             return <BasicPost post={blog} key={id} left={true} />;
           })}
         </div>
+        <ReactPaginate
+          nextLabel="next"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          pageCount={pageCount}
+          previousLabel="prev"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+          disabledClassName="disable"
+          renderOnZeroPageCount={null}
+        />
       </div>
     </Wrapper>
   );
@@ -87,12 +128,43 @@ const Wrapper = styled.section`
     }
     .blog-post-section {
       display: grid;
-      margin: 0 auto;
+      margin: 2rem auto;
       column-gap: 2rem;
       .post-container {
         display: grid;
         grid-template-columns: 1fr;
         margin-top: 2rem;
+      }
+    }
+    .pagination {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      list-style: none;
+      cursor: pointer;
+      .active {
+        background: var(--secondary-700);
+        a {
+          color: white;
+        }
+      }
+      .disable {
+        a {
+          color: var(--grey-clr);
+        }
+      }
+
+      .page-item {
+        margin: 0;
+      }
+      .page-link {
+        width: 100%;
+        padding: 0.5rem;
+        font-size: 0.85rem;
+        display: inline-block;
+      }
+      .page-item {
+        border: 1px solid var(--secondary-500);
       }
     }
   }
@@ -119,6 +191,12 @@ const Wrapper = styled.section`
     .blog-post-section {
       grid-template-columns: 1fr 1fr 1fr;
     }
+    .pagination {
+      .page-link {
+        padding: 0.5rem 1rem !important;
+        font-size: 0.85rem !important;
+      }
+    }
   }
   @media ${devices.tabletL} {
     .blog-header {
@@ -126,8 +204,6 @@ const Wrapper = styled.section`
         height: 370px;
         max-height: 370px !important;
       }
-    }
-    .blog-post-section {
     }
   }
 `;
